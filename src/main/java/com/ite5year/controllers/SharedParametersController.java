d56f751027caf5330db8e76e52d45cc12a2bcd29
Abdulrahman Falyoun <abdulrahman-falyoun@outlook.com>
2020-11-18 10:39:28 +0200

    Fixing

--- src/main/java/com/ite5year/controllers/SharedParametersController.java
+++ src/main/java/com/ite5year/controllers/SharedParametersController.java
@@ -2,6 +2,8 @@ package com.ite5year.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,10 +16,15 @@ public class SharedParametersController {
     @Resource(name="sharedParametersMap")
     private Map<String, Object> parametersMap;
 
-    @RequestMapping("/put.service")
+   @PutMapping("/put-params")
     public @ResponseBody
     Map<?,?> put(String key, String value){
         this.parametersMap.put(key, value);
         return this.parametersMap;
+    }
+
+    @GetMapping("/")
+    public @ResponseBody Map<String, Object> getMap() {
+       return this.parametersMap;
     }
 }
